c658db07c13e08ccccfcd968f61543e02b367121
Miguel Michel <migmichel27@gmail.com>
2019-03-04 13:49:04 -0600

    Clase pelicula cambio 2

--- Pelicula.java
+++ Pelicula.java
@@ -1,5 +1,10 @@
-public class Pelicula(){
+public class Pelicula{
 	//Atributos de la clase
 	private String nombre;
 	private String director;
+
+	public Pelicula (String nombre, String director){
+	this.nombre=nombre;
+	this.director=director;	
+	}
 }
